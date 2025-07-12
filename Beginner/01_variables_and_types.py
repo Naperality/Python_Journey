2f1ce0376761eabd39362bfa335a882030eb11d3
Naperality <napolytan@gmail.com>
2025-07-12 13:56:54 +0800

    Day 1: Variables and Basic inputs and output

--- Beginner/01_variables_and_types.py
+++ Beginner/01_variables_and_types.py
@@ -47,5 +47,70 @@ sampleStr = "Nigeria"
 print(sampleStr[4]) # outputs 'r'
 print(sampleStr[-3]) # outputs 'r'
 
+# however, str are not mutable or immutable for 
+# individual character change but a str can be change value with another str
+# sampleStr[0] = 't' shows error assignment
+sampleStr = 'Philippines is Good'
+
 # String len() shows the length of the string including whitespaces
 print(len(sampleStr+" "))
+
+# str.find() finds the specific character or string in the string
+print(sampleStr.find('n')) #outputs -1 since no 'n' but 'N' is 0
+
+#str.lower() lowercase the str and str.islower() return true if it is all lowercase
+print(sampleStr.lower().islower())
+#likewise to upper() and isupper()
+print(sampleStr.upper().isupper())
+
+#Code exercise Caesar Code using .find() and lower() or upper() and for loop
+# alphabet = 'abcdefghijklmnopqrstuvwxyz'
+# shift = 3
+def caesar(str, offset, direction = 1):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    encrypted_str = ''
+    for letter in str.lower():
+        #for whitespace to skip use continue or acccept white space
+        if letter == ' ':
+            encrypted_str += letter
+            continue
+        index = alphabet.find(letter)
+        # print(letter,index)
+        new_index = (index + offset * direction) % len(alphabet)
+        encrypted_str += alphabet[new_index]
+        # print(f"letter: {letter}, new letter: {encrypted_str}")
+    # print(f'Original Text: {str}')
+    # print(f'Encrypted Text: {encrypted_str}')
+    return encrypted_str
+encrypted = caesar(sampleStr, 3, -1)
+decrypted = caesar(encrypted, 3)
+print(encrypted)
+print(decrypted)
+# caesar(sampleStr,13)
+
+# Vigenere cipher code
+def vigenere(str,key, direction = 1):
+    key_index = 0
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    encrypted_text = ''
+    for letter in str.lower():
+        # add space to the message
+        if letter == ' ':
+            encrypted_text += letter
+            continue
+        # Find the right key character to encode
+        key_char = key[key_index % len(key)]
+        key_index += 1
+        # define the offset  and letter
+        offset = alphabet.index(key_char)
+        index = alphabet.find(letter)
+        new_index = (index + offset * direction) % len(alphabet)
+        encrypted_text += alphabet[new_index]
+    return encrypted_text
+
+text = 'Hello Zaira'
+custom_key = 'python'
+encryption = vigenere(text, custom_key, 1)
+print(encryption)
+decryption = vigenere(encryption, custom_key, -1)
+print(decryption)
