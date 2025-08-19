10b89bcfdfa714c9a1876c1c14ccfa70a7f6063f
Naperality <napolytan@gmail.com>
2025-08-19 22:35:49 +0800

    add: swapcase

--- hackerRankExercises/swapcase.py
+++ hackerRankExercises/swapcase.py
@@ -28,3 +28,10 @@
 
 # hACKERrANK.COM PRESENTS "pYTHONIST 2".
 
+def swap_case(s):
+    return ''.join([letter.lower() if letter==letter.upper() else letter.upper() for letter in s])
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
