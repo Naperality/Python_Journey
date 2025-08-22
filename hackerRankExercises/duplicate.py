ced72f71e0cadb10bcb3d3b4c4d50f169555e39c
Naperality <napolytan@gmail.com>
2025-08-22 17:56:21 +0800

    add: duplicate

---
+++ hackerRankExercises/duplicate.py
@@ -0,0 +1,14 @@
+lst = [1,2,2,2,4,5,1,2,3,4,5,6]
+
+def find_duplicate(lst):
+    seen = set()
+    duplicates = set()
+    for num in lst:
+        if num in seen:
+            duplicates.add(num)
+        else:
+            seen.add(num)
+    return list(duplicates)
+print(find_duplicate(lst))
+# Another approach using collections.Counter
+
